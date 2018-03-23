40113fbe3000531d31af3c9603b2b6841261722a
Igor Konobas <kois9911@gmail.com>
2018-03-23 23:31:17 +0200

    Library class added

--- KnuDbWithEf/Form1.Designer.cs
+++ KnuDbWithEf/Form1.Designer.cs
@@ -29,12 +29,28 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.eMPLOYEEBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.eMPLOYEEBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // eMPLOYEEBindingSource
+            // 
+            this.eMPLOYEEBindingSource.DataSource = typeof(EmployeeEf.EMPLOYEE);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.eMPLOYEEBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.BindingSource eMPLOYEEBindingSource;
     }
 }
 
